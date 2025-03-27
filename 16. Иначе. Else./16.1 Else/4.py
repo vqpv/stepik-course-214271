9506053ac2b675259e7890160d7d30b943ccb36c
RAX3R <45417916+vqpv@users.noreply.github.com>
2025-03-27 21:33:40 +0300

    16.1 - 4

---
+++ 16. Иначе. Else./16.1 Else/4.py
@@ -0,0 +1,10 @@
+a = int(input())
+b = int(input())
+c = int(input())
+
+count = 4
+
+if a + b + c < count:
+    print("Можно ещё поиграть!")
+else:
+    print("Игр достаточно, лучше пойти кодить!")
